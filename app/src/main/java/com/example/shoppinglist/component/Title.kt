dc7e6dc5edacfe07a31aef5aedcbf95fade4a0d5
AlyaZulhanifa <alyazulhanifa48916@gmail.com>
2025-09-16 17:48:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/shoppinglist/component/Title.kt
@@ -0,0 +1,27 @@
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Tittle() {
+    Text(
+        text = "Shopping LIst",
+        style = MaterialTheme.typography.headlineLarge.copy (
+            fontWeight = FontWeight.Bold,
+            fontSize = 32.sp,
+            color = MaterialTheme.colorScheme.primary
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 16.dp),
+        textAlign = TextAlign.Center
+    )
+}
